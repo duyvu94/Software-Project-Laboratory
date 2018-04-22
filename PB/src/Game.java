27e4d04c6218b654cd404a8a42eda3e40190e5f9
Vu Phuong Duy <duysp1994@gmail.com>
2018-04-22 23:03:37 +0200

    update new code

--- PB/src/Game.java
+++ PB/src/Game.java
@@ -16,11 +16,16 @@ public class Game {
 	}
 	
 	public void ChooseLevel(int level, int numberOfPlayers) throws IOException{
+		
+	}
+	
+	public void LoadMap(String map) throws IOException{
 		storage = new Storage();
 		
-		BufferedReader br = new BufferedReader(new FileReader("Map"+ level +".txt"));
+		BufferedReader br = new BufferedReader(new FileReader(map));
 		try {
 			String line = br.readLine();
+			storage.LoadFields(line);
 
 		} finally {
 		    br.close();
@@ -28,16 +33,18 @@ public class Game {
 	}
 	
 	public static void main(String args[]) throws IOException{
+		Game game = new Game();
 		Scanner scanner = new Scanner(System.in);
 		while (true){
 			String text = scanner.nextLine();
 			String command[] = text.split(" ");
 			switch (command[0]){
 				case "LoadMap":
-					
+					game.LoadMap(command[1]);
 					System.out.println("Map is loaded!");
 					break;
-				case "":
+				case "Connect":
+					
 					break;
 				default:
 					System.out.println("Unknown command!");
